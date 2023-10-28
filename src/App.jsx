1fd7374096d5c245b5c63c6057b1fdc1b29f08c9
Daniel Denkha <danialdnkha@gmail.com>
2023-10-28 17:47:24 +1100

    creating and styling hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,26 @@
+import illustration from '../../assets/images/illustration-working.svg';
+import styles from './hero.module.css';
+function Hero() {
+  return (
+    <div className={styles.heroWrapper}>
+      <div className={styles.heroDetails}>
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand&apos;s recognition and get detailed insights on how
+          your links are performing.
+        </p>
+        <a href='#' className={styles.heroBtn}>
+          Get Started
+        </a>
+      </div>
+
+      <img
+        src={illustration}
+        className={styles.heroIllustration}
+        alt='illustration image'
+      />
+    </div>
+  );
+}
+
+export default Hero;
